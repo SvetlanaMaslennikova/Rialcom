92b6ef82435c9cf41f761c695e61d652699e35c9
Svetlana Maslennikova <svetlana-pavlova1005@mail.ru>
2023-02-22 12:45:56 +0300

    create views

--- connection_contract/contracts/views.py
+++ connection_contract/contracts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Contract, ConnectionAddress
+from .serializers import ContractModelSerializer, ConnectionAddressModelSerializer
+from rest_framework.views import APIView
+
+
+class ContractPaginator(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ContractModelViewSet(ModelViewSet):
+    queryset = Contract.objects.all()
+    serializer_class = ContractModelSerializer
+    pagination_class = ContractPaginator
+
+
+class ConnectionAddressModelViewSet(ModelViewSet):
+    queryset = ConnectionAddress.objects.all()
+    serializer_class = ConnectionAddressModelSerializer
+
