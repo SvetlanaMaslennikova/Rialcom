6b7d822a63958a5c9000750ca80162a912ab5623
Svetlana Maslennikova <svetlana-pavlova1005@mail.ru>
2023-02-22 12:31:57 +0300

    create serializers

---
+++ connection_contract/contracts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Contract, ConnectionAddress
+
+
+class ContractModelSerializer(ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = '__all__'
+
+
+class ConnectionAddressModelSerializer(ModelSerializer):
+    model = ConnectionAddress
+    fields = '__all__'
